719a7ba427780803b3b2673fdc08a04844a55eca
Inzamam <inzamam@credencesoft.in>
2022-07-25 17:59:45 +0700

    Created Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
+import { IonButton, IonContent, IonImg, IonPage } from '@ionic/react';
 import './Home.css';
 
 const Home = () => {
   return (
-    <h2>Home</h2>
+    <IonPage>
+      <IonImg className='background-image' src='/Home_Page.svg' alt="image" />
+      <IonContent className='content ion-text-center'>
+        <IonImg className='logo' src="/Logo.svg" alt="" />
+        <h1>Find Your</h1>
+        <h2>Dream House</h2>
+        <h4>Get free consultation today!</h4>
+        <IonButton className='button'>SIGN UP / LOG IN</IonButton>
+      </IonContent>
+    </IonPage>
   );
 };
 
